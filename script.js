b78088c0eb1459f50665429e7e458ded885f349b
Mayara <mayaracamargomc@gmail.com>
2019-02-14 22:48:51 -0200

    tentar fazer essa atividade

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const aleatorio = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1))+min;
-}
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+console.log(aleatorio(0,50));
 
+let numUsuario = Number(prompt('Digite um número'));
+
+if (numUsuario==aleatorio){
+    alert('Parabéns, você acertou!');
+}
+else {
+        alert('Você errou, tente novamente.')
+    }
